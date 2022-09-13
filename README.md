ac2e40883aaca9e9fb52c9ee2edf42ae943248de
garvageart <45718863+garvageart@users.noreply.github.com>
2022-09-13 22:12:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website Application ([lesis.online](https://lesis.online))
+
+This repository containts the assets for my portfolio website and the [Node.js](https://nodejs.org/) application that does [image processing](/libs/photo_processor) for all my photos, as well as, all the data gathering for the data that is displayed on the website.
---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Updates time every second
+setInterval(() => {
+    const dateObject = new Date();
+    const dateString = dateObject.toLocaleString("en-ZA", { dateStyle: "long" });
+    const timeString = dateObject.toLocaleString("en-ZA", { timeStyle: "short" });
+
+    const dateText = `${dateString}\n${timeString}`;
+    document.getElementById("header-date").innerText = dateText;
+}, 1000);
